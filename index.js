43b3e9e0b188ca8c5ffa8981fd1bf773b2a77f7c
Linda ONeill <oneill.linda21@gmail.com>
2017-11-09 16:23:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) {return kittens.push("Ralph")}
+function destructivelyPrependKitten(name) {return kittens.unshift("Ralph")}
