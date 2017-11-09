7f881e0bce88c1fc2a892d42750b6ce5e10d8a1f
Linda ONeill <oneill.linda21@gmail.com>
2017-11-09 16:29:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ function destructivelyAppendKitten(name) {return kittens.push("Ralph")}
 function destructivelyPrependKitten(name) {return kittens.unshift("Bob")}
 function destructivelyRemoveLastKitten() {return kittens.pop(3)}
 function destructivelyRemoveFirstKitten() {return kittens.shift(0)}
-function appendKitten(name) {return [kittens, "broom"]}
+function appendKitten(name) {return [kittens, "Broom"]}
